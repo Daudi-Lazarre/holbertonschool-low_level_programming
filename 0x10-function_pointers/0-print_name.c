e2b10b3732e9b047cf14ea028e21baadcc0ac75d
Lucas Lazarre <lucaslazarre@DaudiLazarre.local>
2021-07-27 17:27:47 -0400

    reuploading 0

--- 0x10-function_pointers/0-print_name.c
+++ 0x10-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Print the name of a function
- * @name: The name
- * @f: Function Pointer
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to print with
+ *
  * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		return;
-	f(name);
+		f(name);
 }
