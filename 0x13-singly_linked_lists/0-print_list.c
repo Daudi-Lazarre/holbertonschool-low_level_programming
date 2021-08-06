9dcbc5236e9fa5bf9a3a515afcc0f1f0e8b26184
Lucas Lazarre <lucaslazarre@DaudiLazarre.local>
2021-08-06 19:38:59 -0400

    Breaking down code and rebuilding it: add

--- 0x13-singly_linked_lists/0-print_list.c
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -11,15 +11,22 @@ size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
+
+	/* Increment through num && hop to the next pointer when... */
+	/* the node pointer is not the last one */
+	/* If the string node is NULL, print this. */
+	/* In all other cases, print the node digit and string. */
+	do
+	{
+		num++;
+		h = h->next;
+	}
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		num++;
-		h = h->next;
 	}
 	return (num);
 }
